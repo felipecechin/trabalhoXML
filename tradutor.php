7a6c235171d39ead16882e7bce71947c1eefb086
Felipe Cechin <ficechin@hotmail.com>
2019-11-04 23:27:01 -0300

    Criando links para associações.

--- tradutor.php
+++ tradutor.php
@@ -10,11 +10,18 @@ $xml_string = $xml->saveXML();
 $xml = new SimpleXMLElement($xml_string);
 
 if (!is_dir('filmes')) {
-    // dir doesn't exist, make it
     mkdir('filmes');
 }
 
 $ocorrencias = ['#sinopse' => 'Sinopse', '#ingles' => 'Título em inglês', '#elencoApoio' => 'Elenco de apoio', '#site' => 'Site', '#distribuicao' => 'Distribuição'];
+$associacoes = ['#filme-ano' => 'Ano', '#filme-elenco' => 'Elenco', '#filme-direcao' => 'Direção', '#filme-genero' => 'Gênero', '#filme-duracao' => 'Duração'];
+
+foreach ($associacoes as $chave => $valor) {
+    $chave = str_replace('#filme-', '', $chave);
+    if (!is_dir($chave)) {
+        mkdir($chave);
+    }
+}
 
 $buscaFilmes = "//topic[./instanceOf/topicRef/@href='#Filme']";
 $results = $xml->xpath($buscaFilmes);
@@ -51,6 +58,17 @@ foreach($results as $item){
             }
             $paginaFilme .= '<li>'.$tipo.': '.$valor.'</li>';
         }
+    }
+    $paginaFilme .= '</ul><h3>Associações</h3><ul>';
+    $buscaAssociacoes = '//association[./member[1]/topicRef/@href = "#'.$item["id"].'"]';
+    $results2 = $xml->xpath($buscaAssociacoes);
+    foreach ($results2 as $item2) {
+        $tipo = $item2->instanceOf->topicRef['href']->__toString();
+        $nomePasta = str_replace('#filme-', '', $tipo);
+        $tipo = $associacoes[$tipo];
+        $idTipo = $item2->member[1]->topicRef['href']->__toString();
+        $idTipo = str_replace('#', '', $idTipo);
+        $paginaFilme .= '<li><a href="../'.$nomePasta.'/'.$idTipo.'">'.$tipo.'</a></li>';
     }
     $paginaFilme .= '</ul></body></html>';
     file_put_contents($fileFilme, $paginaFilme);
